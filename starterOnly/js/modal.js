4174159ffad8845a9a79d7b70f4417b235284cbc
Annabelle Braye <annabelle.braye@gmail.com>
2023-08-24 10:06:43 +0200

    feat(implement-entries): Work on issue 'implement entries', correction of HTML file, add required class, create validate() function to check input on submit.

--- starterOnly/js/modal.js
+++ starterOnly/js/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const formElt = document.querySelector('[name=reserve]');
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -19,5 +20,31 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+// Check on submit
+function validate() {
+  const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+
+  const firstnameOk = formElt.firstname.value.length >= 2;
+  
+  const lastnameOk = formElt.lastname.value.length >= 2;
+
+  const emailOk = regex.test(formElt.email.value)
+  
+  const qtyValue = formElt.quantity.value;
+  const qtyOk = !qtyValue || (qtyValue && !isNaN(qtyValue) && qtyValue > 0)
+
+  const radioOk = Array.from(formElt.location).find(x => x.checked) !== undefined
+
+  const conditionsOk = formElt.checkbox1.checked
+
+  if (firstnameOk && lastnameOk && emailOk && qtyOk && radioOk && conditionsOk) {
+    return true
+  } else {
+    console.log(`Les champs ne sont pas correctement remplis`);
+    return false;
+  }
+}
+
 
 
